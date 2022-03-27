82ac43df6c51837a273a965731fdc3904202e01c
YihengJIAN <jianyiheng@outlook.com>
2022-03-26 20:58:39 -0700

    Rebuild NVU lut approximation (unverified)

--- src/main/scala/nvu/common/Nvu_lut.scala
+++ src/main/scala/nvu/common/Nvu_lut.scala
@@ -3,8 +3,14 @@ package nvu.common
 import spinal.core._
 import spinal.lib._
 
-class Nvu_lut extends Component {
+import scala.util.Random
+import scala.math
+
+case class Nvu_lut() extends Component {
   val io = new Bundle {
+    val x_vld = in Bool()
+    val y_vld = out Bool()
+
     val lut_x = in Vec(SFix(Nvu_params.LUT_X_INTG exp, -Nvu_params.LUT_X_FRAC exp), Nvu_params.LUT_X_LEN)
     val lut_y = in Vec(SFix(Nvu_params.LUT_Y_INTG exp, -Nvu_params.LUT_Y_FRAC exp), Nvu_params.LUT_Y_LEN)
     val lut_k = in Vec(SFix(Nvu_params.LUT_K_INTG exp, -Nvu_params.LUT_K_FRAC exp), Nvu_params.LUT_X_LEN)
@@ -13,36 +19,58 @@ class Nvu_lut extends Component {
     val y_i   = out SFix(Nvu_params.LUT_OUT_INTG exp, -Nvu_params.LUT_OUT_FRAC exp)
   }
 
-  val lut_pos_ary  = Reg(Bits(Nvu_params.LUT_X_LEN bits)) init(0)
-  val lut_pos      = CountOne(lut_pos_ary) - 1
+  val lut_pos_ary = Reg(Bits(Nvu_params.LUT_X_LEN bits))
+  val lut_pos     = CountOne(lut_pos_ary)-1
 
-  val lut_x_p      = io.lut_x(lut_pos)
-  val lut_y_p      = io.lut_y(lut_pos)
-  val lut_y_p_1    = io.lut_y(lut_pos+1)
-  val lut_k_p      = io.lut_k(lut_pos)
+  val lut_x_r     = Reg(SFix(Nvu_params.LUT_X_INTG exp, -Nvu_params.LUT_X_FRAC exp))
+  val lut_y_r     = Reg(SFix(Nvu_params.LUT_Y_INTG exp, -Nvu_params.LUT_Y_FRAC exp))
+  val lut_y_r_1   = Reg(SFix(Nvu_params.LUT_Y_INTG exp, -Nvu_params.LUT_Y_FRAC exp))
+  val lut_k_r     = Reg(SFix(Nvu_params.LUT_K_INTG exp, -Nvu_params.LUT_K_FRAC exp))
 
-  val delta_one    = Reg(SFix(Nvu_params.LUT_DELTA_INTG exp, -Nvu_params.LUT_DELTA_FRAC exp)) init (1)
-  val delta_minus  = Reg(SFix(Nvu_params.LUT_DELTA_INTG exp, -Nvu_params.LUT_DELTA_FRAC exp)) init (0)
-  val delta        = Reg(SFix(Nvu_params.LUT_DELTA_INTG exp, -Nvu_params.LUT_DELTA_FRAC exp)) init (0)
+  val delta       = SFix(Nvu_params.LUT_DELTA_INTG exp, -Nvu_params.LUT_DELTA_FRAC exp)
+  val delta_one   = SFix(Nvu_params.LUT_DELTA_INTG+1 exp, -Nvu_params.LUT_DELTA_FRAC exp)
+  val delta_a     = Reg(SFix(Nvu_params.LUT_DELTA_INTG exp, -Nvu_params.LUT_DELTA_FRAC exp))
+  val delta_b     = Reg(SFix(Nvu_params.LUT_DELTA_INTG exp, -Nvu_params.LUT_DELTA_FRAC exp))
 
-  for (i <- 0 until Nvu_params.LUT_X_LEN) {
-    when (io.x_i > io.lut_x(i)) {
-      lut_pos_ary(i)  := True
-    }.otherwise {
-      lut_pos_ary(i)  := False
+  when (io.x_vld) {
+    for (i <- 0 until Nvu_params.LUT_X_LEN) {
+      when (io.x_i > io.lut_x(i)) {
+        lut_pos_ary(i)  := True
+      }.otherwise {
+        lut_pos_ary(i)  := False
+      }
     }
   }
 
-  delta       := ((io.x_i - lut_x_p) * lut_k_p).truncated
-  delta_minus := (delta_one - delta).truncated
+  when (Delay(io.x_vld, 1)) {
+    lut_x_r   := io.lut_x(lut_pos)
+    lut_y_r   := io.lut_y(lut_pos)
+    lut_y_r_1 := io.lut_y(lut_pos+1)
+    lut_k_r   := io.lut_k(lut_pos)
+  }.otherwise {
+    lut_x_r   := lut_x_r
+    lut_y_r   := lut_y_r
+    lut_y_r_1 := lut_y_r_1
+    lut_k_r   := lut_k_r
+  }
 
-  io.y_i := (delta * lut_y_p + delta_minus * lut_y_p_1).truncated
+  delta     := ((Delay(io.x_i, 2) - lut_x_r) * lut_k_r).truncated
+  delta_one := 1
+
+  when (Delay(io.x_vld, 2)) {
+    delta_a := delta
+    delta_b := (delta_one - delta).truncated
+  }
+
+  io.y_i := Vec(delta_a, delta_b).zip(Vec(lut_y_r, lut_y_r_1)).map{case(a, b)=>RegNext(a * b)}.reduceBalancedTree(_+_,(s,l)=>RegNext(s)).truncated
+
+  io.y_vld := Delay(io.x_vld, 6)
 
 }
 
 object Nvu_lut_main {
   def main(args: Array[String]) {
-    SpinalConfig(targetDirectory = "rtl").generateVerilog(Nvu_lut)
+    SpinalConfig(targetDirectory = "rtl").generateVerilog(Nvu_lut())
   }
 }
 
